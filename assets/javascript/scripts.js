569921f0f7c423fdf370279f4450cbd8bc113c3c
Talha <106121597+WebDevTalha@users.noreply.github.com>
2022-06-30 23:38:44 -0700

    File setup done

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  
+  // Nav Bar
+  $(".menu-bar").click(function(){
+    $(".menu").slideToggle("slow");
+  });
+
+});
+
+$(document).ready(function() {
+  $('#counter span').counterUp({
+      time: 1000
+  });
+});
+
+
